2e6006ccbe7f8b309b141f00f0bf4fe7609f5267
yuanzy <yuanzhiyi98@gmail.com>
2025-04-21 22:55:00 +0800

    feat(core-shadcn-ui):重构登录表单组件
    - 更新了登录表单的样式和布局
    - 添加了 Apple 和 Google 登录按钮
    - 增加了用户协议和隐私政策链接- 调整了表单字段的样式
    - 优化了响应式布局

--- apps/app-demo/app/page.tsx
+++ apps/app-demo/app/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from "@repo/core-shadcn-ui/components/ui/button";
+import {LoginForm} from "@repo/core-shadcn-ui/components/login-form";
 
 export default function Page() {
   return (
     <main>
-      <Button>Click me</Button>
+      <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <a href="#" className="flex items-center gap-2 self-center font-medium">
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            </div>
+            Acme Inc.1
+          </a>
+          <LoginForm />
+        </div>
+      </div>
     </main>
   );
 }
