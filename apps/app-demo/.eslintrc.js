ac09f910bf98cb95d6ba7daae13a4495096ec046
yuanzy <yuanzhiyi98@gmail.com>
2025-05-24 20:18:34 +0800

    build:重构项目并移除 ESLint 配置
    - 删除了多个 ESLint 配置文件，包括 .eslintrc.js 和 react-internal.js 等
    - 移除了 ESLint相关的 devDependencies
    - 更新了 pnpm-workspace.yaml，添加了全局可用的脚本
    - 删除了多个 README.md 文件，清理了不必要的文档
    - 更新了 package.json 文件，统一了构建和测试脚本

--- apps/app-demo/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@repo/core-config-eslint/next.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
--- packages/core-shadcn-ui/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ['@repo/core-config-eslint/react-internal.js'],
-  parser: '@typescript-eslint/parser',
-  rules: {
-    'no-redeclare': 'off',
-  },
-};
