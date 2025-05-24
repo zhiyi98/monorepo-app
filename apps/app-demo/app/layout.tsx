bc9260e1c123fb6686b427a018da46f365d324d3
yuanzy <yuanzhiyi98@gmail.com>
2025-05-24 21:38:16 +0800

    refactor(app-demo): 优化 layout 组件返回类型
    - 引入 React 类型，明确组件返回值类型为 React.ReactNode
    -优化类型定义，提高代码可读性和类型安全性

--- apps/app-demo/app/layout.tsx
+++ apps/app-demo/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@repo/core-shadcn-ui/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,7 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}): JSX.Element {
+}): React.ReactNode {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
